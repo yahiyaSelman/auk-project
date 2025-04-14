17441503cfc4220e16a88402c6f7ab3aba5a2eec
Yahiya Selman <47330014+yahiyaSelman@users.noreply.github.com>
2025-04-14 14:39:17 +0300

    Update retrieval.js

--- retrieval.js
+++ retrieval.js
@@ -1,1 +1,9 @@
+import fs from 'fs';
+import path from 'path';
 
+export const searchDocuments = (query) => {
+  const dataPath = path.join(process.cwd(), './lib/resources.json');
+  const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+
+  return data.filter(doc => doc.content.toLowerCase().includes(query.toLowerCase()));
+};
